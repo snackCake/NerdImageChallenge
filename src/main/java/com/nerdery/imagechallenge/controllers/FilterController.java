8c13daf758405b7856945d5a6377987bfc193fb2
Josh Klun <jklun@nerdery.com>
2015-07-06 18:12:28 -0500

    Initial commit

---
+++ src/main/java/com/nerdery/imagechallenge/controllers/FilterController.java
@@ -0,0 +1,65 @@
+package com.nerdery.imagechallenge.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletContext;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.stream.IntStream;
+
+@Controller
+public class FilterController {
+
+    @Autowired
+    ServletContext context;
+
+    @RequestMapping("/filter")
+    public ResponseEntity<byte[]> testphoto() throws IOException, URISyntaxException {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        BufferedImage sourceImage = ImageIO.read(new URL("http://media.bizj.us/view/img/923121/nerderynoodlerkensykora*750.jpg"));
+        BufferedImage targetImage = transformImage(sourceImage);
+        BufferedImage finalImage = buildFinalComposite(sourceImage, targetImage);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(finalImage, "JPEG", outputStream);
+        return new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.CREATED);
+    }
+
+    private BufferedImage transformImage(BufferedImage sourceImage) {
+        int sourceWidth = sourceImage.getWidth();
+        int sourceHeight = sourceImage.getHeight();
+        BufferedImage targetImage = new BufferedImage(sourceWidth, sourceHeight, sourceImage.getType());
+        IntStream.range(0, sourceHeight).parallel().forEach(y -> IntStream.range(0, sourceWidth).forEach(x -> {
+            int pixel = sourceImage.getRGB(x, y);
+            int channel2 = pixel & 0x00FF0000;
+            int channel3 = pixel & 0x0000FF00;
+            int swappedPixel = (channel2 >> 8) | (channel3 << 8);
+            targetImage.setRGB(x, y, swappedPixel);
+
+        }));
+        return targetImage;
+    }
+
+    private BufferedImage buildFinalComposite(BufferedImage sourceImage, BufferedImage targetImage) {
+        int sourceHeight = sourceImage.getHeight();
+        int sourceWidth = sourceImage.getWidth();
+        BufferedImage finalImage = new BufferedImage(sourceWidth, sourceHeight * 2 + 1, sourceImage.getType());
+        Graphics graphics = finalImage.getGraphics();
+        graphics.drawImage(sourceImage, 0, 0, null);
+        graphics.setColor(Color.RED);
+        graphics.drawLine(0, sourceHeight, sourceWidth, sourceHeight);
+        graphics.drawImage(targetImage, 0, sourceHeight + 1, null);
+        return finalImage;
+    }
+}
