69454befa8cbcfc0b1e57c60430dcd815e54a05c
Josh Klun <jklun@nerdery.com>
2015-08-03 10:08:30 -0500

    Add support for multiple file formats (JPG, PNG, GIF).

--- src/main/java/com/nerdery/imagechallenge/controllers/FilterController.java
+++ src/main/java/com/nerdery/imagechallenge/controllers/FilterController.java
@@ -1,10 +1,14 @@
 package com.nerdery.imagechallenge.controllers;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableMap;
 import com.nerdery.imagechallenge.services.CompositeService;
 import com.nerdery.imagechallenge.services.FilterService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import static org.springframework.http.MediaType.*;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,6 +22,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -26,26 +31,48 @@ import java.util.Optional;
 @Controller
 public class FilterController {
 
+    private static final String DEFAULT_FILE_FORMAT = IMAGE_JPEG.getSubtype();
+    private static final Map<String, MediaType> SUPPORTED_FILE_FORMATS = new ImmutableMap.Builder<String, MediaType>()
+            .put(IMAGE_JPEG.getSubtype(), IMAGE_JPEG)
+            .put(IMAGE_GIF.getSubtype(), IMAGE_GIF)
+            .put(IMAGE_PNG.getSubtype(), IMAGE_PNG)
+            .build();
+
     private CompositeService compositeService;
     private FilterService filterService;
     private String sourceUrl;
 
     @RequestMapping("/filter/{name}")
     public ResponseEntity<byte[]> getTransformedImage(@PathVariable("name") String filterName) throws IOException, URISyntaxException {
+        return buildResponseEntity(filterName, DEFAULT_FILE_FORMAT);
+    }
+
+    @RequestMapping("/filter/{name}/{format}")
+    public ResponseEntity<byte[]> getTransformedImage(@PathVariable("name") String filterName, @PathVariable("format") String fileFormat)
+            throws IOException, URISyntaxException {
+        String normalizedFormat = fileFormat.toLowerCase();
+        if (!SUPPORTED_FILE_FORMATS.containsKey(normalizedFormat)) {
+            String message = "Invalid file format. Valid Formats: [" + Joiner.on(", ").join(SUPPORTED_FILE_FORMATS.keySet()) + "]";
+            return new ResponseEntity<>(message.getBytes(), HttpStatus.BAD_REQUEST);
+        }
+        return buildResponseEntity(filterName, normalizedFormat);
+    }
+
+    private ResponseEntity<byte[]> buildResponseEntity(@PathVariable("name") String filterName, String fileFormat) throws IOException {
         BufferedImage sourceImage = ImageIO.read(new URL(sourceUrl));
         Optional<BufferedImage> targetImage = filterService.transformImage(sourceImage, filterName);
         if (targetImage.isPresent()) {
-            return buildSuccessResponse(compositeService.buildComposite(sourceImage, targetImage.get()));
+            return buildSuccessResponse(compositeService.buildComposite(sourceImage, targetImage.get()), fileFormat);
         } else {
             return new ResponseEntity<>("Invalid filter name".getBytes(), HttpStatus.BAD_REQUEST);
         }
     }
 
-    private ResponseEntity<byte[]> buildSuccessResponse(BufferedImage finalImage) throws IOException {
+    private ResponseEntity<byte[]> buildSuccessResponse(BufferedImage finalImage, String fileFormat) throws IOException {
         ByteArrayOutputStream imageByteStream = new ByteArrayOutputStream();
-        ImageIO.write(finalImage, "JPEG", imageByteStream);
+        ImageIO.write(finalImage, fileFormat, imageByteStream);
         final HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentType(SUPPORTED_FILE_FORMATS.get(fileFormat));
         return new ResponseEntity<>(imageByteStream.toByteArray(), headers, HttpStatus.CREATED);
     }
 
